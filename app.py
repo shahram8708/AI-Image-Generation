f964c5c9bfd346184b4f302ea554612f65c0ef8f
shah.ram <ram.coding8@gmail.com>
2024-03-24 10:42:48 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+API_KEY = 'AIzaSyDxdLTyUowK86sjco6AhJZRdWFFEtwyu5Y'
+CX = 'b45dc313d50ac4cef'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    term = request.args.get('term')
+    if not term:
+        return jsonify([])
+    else:
+        google_api_url = f"https://www.googleapis.com/customsearch/v1?q={term}&cx={CX}&key={API_KEY}&searchType=image"
+        response = requests.get(google_api_url)
+        if response.status_code == 200:
+            data = response.json()
+            images = [item['link'] for item in data.get('items', [])]
+            return jsonify(images)
+        else:
+            return jsonify([])
+
+if __name__ == '__main__':
+    app.run(debug=True)
